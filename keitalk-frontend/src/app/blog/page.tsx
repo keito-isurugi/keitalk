ece3b187e4d3c6cd60d096337d0a1abbc90e1f98
KeitoIsurugi <ja224kg@gmail.com>
2023-10-29 12:18:10 +0900

    frontend frist commit

---
+++ keitalk-frontend/src/app/blog/page.tsx
@@ -0,0 +1,29 @@
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
+
+async function getData() {
+  const res = await fetch('http://localhost:8088/api/articles')
+ 
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+export const Page = async () => {
+  const data = await getData();
+  return (
+    <div>
+      {data.map((d: {
+        id: ReactNode; 
+        title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | PromiseLikeOfReactNode | null | undefined; 
+}) => (
+        <div>
+          {d.id}: { d.title }
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Page
