7d14784a66fcc2db8cb96118dff243601a6600f2
KeitoIsurugi <ja224kg@gmail.com>
2023-10-30 08:34:00 +0900

    ブログ画像テーブル作成

---
+++ keitalk-backend/database/seeders/ArticleImageSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ArticleImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            DB::table("article_images")->insert([
+                    [
+                        "article_id" => $i, 
+                        "image_path" => "", 
+                        "created_at" => now(), 
+                        "updated_at" => now()
+                    ],
+                ]);
+        }
+    }
+}
--- keitalk-backend/database/seeders/DatabaseSeeder.php
+++ keitalk-backend/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
            CategorySeeder::class,
-           ArticleSeeder::class 
+           ArticleSeeder::class,
+           ArticleImageSeeder::class 
         ]);
     }
 }
